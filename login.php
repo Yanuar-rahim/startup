60d08db9380497ceb7d155402fcf1bb4dce95d89
cintadeliaaa <cintadelia06@gmail.com>
2025-06-03 18:47:31 +0800

    gamon

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+    header("Location: beranda.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <form action="proses_login.php" method="post">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
